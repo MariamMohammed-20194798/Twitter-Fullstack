de73a94a60d8bf7037d9755962ff4f4a782fce3d
Mariam Mohammed <74053841+MariamMohammed-20194798@users.noreply.github.com>
2023-12-16 16:04:22 +0200

    Update Tweet.js

--- client/src/Components/Tweet/Tweet.js
+++ client/src/Components/Tweet/Tweet.js
@@ -1,9 +1,9 @@
 import { React, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Modal from "@mui/material/Modal";
-
 import Typography from "@mui/material/Typography";
-import { FiMessageSquare, FiRepeat, FiHeart, FiShare } from "react-icons/fi";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { FiMessageSquare, FiRepeat, FiShare } from "react-icons/fi";
 import instance from "../../axios";
 import {
   P,
@@ -19,14 +19,13 @@ import {
   DivPhotoModal,
   DivRightTweet,
 } from "./TweetStyled";
-const Tweet = ({ text, tweetImg, user, id, numOfLikes, numOfComments }) => {
-  const defaultNumOfLikes = numOfLikes.length;
+
+const Tweet = ({ text, tweetImg, user, id, likes }) => {
   const [userInfo, setUserInfo] = useState([]);
-  const [likes, setLikes] = useState(defaultNumOfLikes);
-  const [comments, setComments] = useState(numOfComments);
   const [openPhoto, setOpenPhoto] = useState(false);
   const handleOpenPhoto = () => setOpenPhoto(true);
   const handleClosePhoto = () => setOpenPhoto(false);
+  const [liked, setLiked] = useState(likes.includes(user._id));
 
   useEffect(() => {
     (async () => {
@@ -39,14 +38,16 @@ const Tweet = ({ text, tweetImg, user, id, numOfLikes, numOfComments }) => {
     })();
   }, [user.username]);
 
-  const handleLikes = async () => {
-    const likesRes = await instance.post(`users/${id}/like`);
-    setLikes(likesRes.data.likes.length);
-  };
-
-  const handlelComments = async () => {
-    const commentsRes = await instance.post(`users/${id}/comments`);
-    setComments(commentsRes.data.comments);
+  const handleLikeDislike = async () => {
+    try {
+      const newLikedStatus = !liked;
+      await instance.post(`users/${id}/like`, {
+        params: { liked: newLikedStatus },
+      });
+      setLiked(newLikedStatus);
+    } catch (error) {
+      console.error("Error liking/disliking tweet:", error);
+    }
   };
 
   return (
@@ -67,7 +68,7 @@ const Tweet = ({ text, tweetImg, user, id, numOfLikes, numOfComments }) => {
             <P>{text}</P>
           </DivText>
           {tweetImg && (
-            <DivImg imageUrl={tweetImg} onClick={handleOpenPhoto}></DivImg>
+            <DivImg $imageurl={tweetImg} onClick={handleOpenPhoto}></DivImg>
           )}
           <Modal
             open={openPhoto}
@@ -83,41 +84,13 @@ const Tweet = ({ text, tweetImg, user, id, numOfLikes, numOfComments }) => {
           </Modal>
 
           <FiDiv>
-            {comments ? (
-              <>
-                <p
-                  style={{
-                    fontSize: "1.2rem",
-                    fontWeight: "bold",
-                  }}
-                >
-                  {comments}
-                  <FiMessageSquare
-                    style={{ color: "#1da1f2" }}
-                    onClick={handlelComments}
-                  />
-                </p>
-              </>
-            ) : (
-              <FiMessageSquare onClick={handlelComments} />
-            )}
+            <FiMessageSquare />
 
             <FiRepeat />
-            {likes ? (
-              <>
-                <p
-                  style={{
-                    fontSize: "1.2rem",
-                    fontWeight: "bold",
-                  }}
-                >
-                  {likes}
-                  <FiHeart style={{ color: "#e0245e" }} onClick={handleLikes} />
-                </p>
-              </>
-            ) : (
-              <FiHeart onClick={handleLikes} />
-            )}
+            <FavoriteBorderIcon
+              onClick={handleLikeDislike}
+              style={{ color: liked ? "#e0245e" : "" }}
+            ></FavoriteBorderIcon>
 
             <FiShare />
           </FiDiv>
