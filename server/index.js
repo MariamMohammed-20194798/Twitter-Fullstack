3613eb8980ac2d9c50720be154df62ebe927045f
Mariam Mohammed <74053841+MariamMohammed-20194798@users.noreply.github.com>
2026-05-16 11:55:22 +0300

    Replace config.env with .env file for environment variables and update database connection in index.js

--- server/index.js
+++ server/index.js
@@ -7,11 +7,10 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "./.env" });
 const app = require("./app");
 
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
+const DB = process.env.DATABASE(
   process.env.DATABASE_PASSWORD
 );
 
